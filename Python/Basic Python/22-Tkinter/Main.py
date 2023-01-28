e756a5fd34d92371620fbe23192509256eedfcce
hammamhanif <101041662+hammamhanif@users.noreply.github.com>
2023-01-28 20:36:37 +0700

    Add files via upload
    Berisikan pembelajaran python saya pribadi untuk menambah pengetahuan programming.

---
+++ Python/Basic Python/22-Tkinter/Main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+window = tk.Tk()
+window.configure(bg='black')
+window.geometry("300x200")
+window.resizable(False, False)
+window.title("Sapa Dia")
+
+# Frame Input
+
+
+window.mainloop()
